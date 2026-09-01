d1c4f246b3e832ba6956b0e62bce84f89b516d39
samirvithlani <samir.vithlani83955@gmail.com>
2026-09-01 11:54:17 +0530

    flex

--- src/components/common/HeroPage.jsx
+++ src/components/common/HeroPage.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export const HeroPage = () => {
   return (
     <>
-    <div className='bg-gray-300 h-[50px] m-5 p-1'>
-        <h1 className='text-blue-500'>HERO PAGE</h1>
-    </div>
-    {/* w-auto
+      <div className="bg-gray-300 h-[50px] m-5 p-1">
+        <h1 className="text-blue-500">HERO PAGE</h1>
+      </div>
+      {/* w-auto
     w-full
     w-screen
     w-min
     w-max */}
-    {/* <div className='bg-yellow-200 h-auto w-auto p-10'>
+      {/* <div className='bg-yellow-200 h-auto w-auto p-10'>
       <h1 className='text-xs'>HELLO</h1>
       <h1 className='text-sm'>HELLO</h1>
       <h1 className='text-base'>HELLO</h1>
@@ -27,14 +27,22 @@ export const HeroPage = () => {
       <h1 className='font-extrabold'>HELLO THIS IS TAIWIND CSS</h1>
     </div>  */}
 
-    <div className='h-[300px] w-[300px] bg-blue-500 ml-50 border-2 border-red-500 rounded-full'>
+      {/* <div className='h-[300px] w-[300px] bg-blue-500 ml-50 border-2 border-red-500 rounded-full'>
 
     </div>
     <div className='h-[300px] w-[300px] bg-yellow-500 ml-50 border-2 border-dotted border-red-500 rounded-full'>
     </div>
 
     <div className='h-[300px] w-[300px] bg-white border-2 shadow-xl shadow-black'>
-    </div>
+    </div> */}
+
+      <div className="flex flex-row gap-2 justify-between">
+        <div className="h-[100px] w-[100px] bg-white border-2 shadow-xl shadow-black"></div>
+        <div className="h-[100px] w-[100px] bg-white border-2 shadow-xl shadow-black"></div>
+        <div className="h-[100px] w-[100px] bg-white border-2 shadow-xl shadow-black"></div>
+        <div className="h-[100px] w-[100px] bg-white border-2 shadow-xl shadow-black"></div>
+        <div className="h-[100px] w-[100px] bg-white border-2 shadow-xl shadow-black"></div>
+      </div>
     </>
-  )
-}
+  );
+};
