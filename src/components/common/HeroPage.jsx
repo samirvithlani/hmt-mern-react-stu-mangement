7bac0756d18bf4cb45560f21b91abf863b7ed1dc
samirvithlani <samir.vithlani83955@gmail.com>
2026-09-01 11:14:32 +0530

    taiwind css

--- src/components/common/HeroPage.jsx
+++ src/components/common/HeroPage.jsx
@@ -2,8 +2,30 @@ import React from 'react'
 
 export const HeroPage = () => {
   return (
-    <div>
-        <h1>HERO PAGE</h1>
+    <>
+    <div className='bg-gray-300 h-[50px] m-5 p-1'>
+        <h1 className='text-blue-500'>HERO PAGE</h1>
     </div>
+    w-auto
+    w-full
+    w-screen
+    w-min
+    w-max
+    <div className='bg-yellow-200 h-auto w-auto p-10'>
+      <h1 className='text-xs'>HELLO</h1>
+      <h1 className='text-sm'>HELLO</h1>
+      <h1 className='text-base'>HELLO</h1>
+      <h1 className='text-lg'>HELLO</h1>
+      <h1 className='text-xl'>HELLO</h1>
+      <h1 className='text-2xl'>HELLO</h1>
+      <h1 className='text-3xl'>HELLO</h1>
+
+      <h1 className='font-thin'>HELLO THIS IS TAIWIND CSS</h1>
+      <h1 className='font-bold'>HELLO THIS IS TAIWIND CSS</h1>
+      <h1 className='font-light'>HELLO THIS IS TAIWIND CSS</h1>
+      <h1 className='font-medium'>HELLO THIS IS TAIWIND CSS</h1>
+      <h1 className='font-extrabold'>HELLO THIS IS TAIWIND CSS</h1>
+    </div> 
+    </>
   )
 }
